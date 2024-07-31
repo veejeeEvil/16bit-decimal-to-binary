59c6a2f2281a87520370cc1edce61ee9dd70d1fc
veejeeEvil <vol.grebenyk2k4@gmail.com>
2024-07-31 03:13:30 +0300

    master

---
+++ main.js
@@ -0,0 +1,25 @@
+let array = []; // 16 symbols
+array.length = 16;
+// WEB REFERENCES
+const input = document.getElementById('input');
+const output = document.getElementById('output');
+
+input.addEventListener("input", toBinary);
+
+// app
+
+output.textContent = '0000000000000000';
+function toBinary(number) {
+    number = input.value;
+    let x = 0;
+    for (let i = 0; i < array.length; i++) {
+        numberDivided = number / 2;
+        numberQtn = Math.floor(numberDivided);
+        remainder = number % 2;
+        array[i] = Math.floor(remainder);
+        number = numberDivided;
+        x = String(array[i] + x);
+    }
+    output.textContent = x;
+    console.log(x);
+}
